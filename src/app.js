72d8e31e66866dc1e02551ecf16c54d7455a5d87
AhmadLobany <ahmad.lobany@campus.technion.ac.il>
2020-05-26 11:15:42 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -4,25 +4,58 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
-  //Your code here
+const findFactorial = function(num) {
+
+  if(num<=1) return 1;
+
+  return num*findFactorial(num-1)
 
 }
 
 //Exercise 2
-const reverseString = function() {
-  //Your code here
+const reverseString = function(str) {
 
+  if(str.length==1) return str[0]
+
+  return str[str.length-1] += reverseString(str.slice(0,str.length-1))
 }
 
 //Exercise 3
-const arr1 = [1, 2, 3]
-const arr2 = []
 
-const swap = function() {
-  //Your code here
 
+const swap = function(arr1,arr2) {
+  if(arr1.length==0) {
+      arr2.reverse()
+      return;
+  }
+  arr2.push(arr1.pop())
+  swap(arr1,arr2)
 }
+
+
+const stack1 = [1, 2, 3]
+const stack2 = []
+
+
+const swapStacks = function(stack1,stack2) {
+  if(stack1.length==0) {
+      return;
+  }
+  stack2.push(stack1.pop())
+  swap(stack1,stack2)
+}
+
+
+swapStacks(stack1, stack2)
+console.log(stack1) //[]
+console.log(stack2) //[1, 2, 3]
+
+
+
+
+
+
+
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
 module.exports = { findFactorial, reverseString, swap }
